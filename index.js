52e0e0919fb5cdbca1fad96c8c29967740d345e4
liujianhua <1769908045@qq.com>
2018-05-13 23:39:48 +0800

    dsd

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const https = require('https')
 const {resolve} = require('path')
-const fs = require('fs')
 const Koa = require('koa')
 const koaBody = require('koa-body')
 const router = require('./router')
 const app = new Koa()
-const ssh = {
-  key: fs.readFileSync(resolve(__dirname, './ssh/ssh.key')),
-  cert: fs.readFileSync(resolve(__dirname, './ssh/ssh.pem'))
-}
 
 app.use(koaBody())
 app.use(router.routes()).use(router.allowedMethods())
-if (process.env.NODE_ENV === 'production') {
-  https.createServer(ssh, app.callback()).listen(8888, () => console.log(`Web Run In https://localhost:8888`))
-} else {
-  app.listen(8888, () => console.log(`Web Run In http://localhost:8888`))
-}
+app.listen(8843, () => console.log(`Web Run In http://localhost:8843`))
