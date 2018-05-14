d329fb7d5b294c31594ab0d6e8d6381f3d06a11d
liujianhua <1769908045@qq.com>
2018-05-14 11:33:22 +0800

    error

--- sql/sqlQuery.js
+++ sql/sqlQuery.js
@@ -4,6 +4,9 @@ const mysql = require('mysql')
 const config = JSON.parse(fs.readFileSync(resolve(__dirname, './DB.json')))
 const connection = mysql.createConnection(config)
 connection.connect()
+connection.on('error', error => {
+  console.log('========' + error.code)
+})
 module.exports = (sql) => {
   return new Promise((resolve, reject) => {
     connection.query(sql, function (err, result) {
