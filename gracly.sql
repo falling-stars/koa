257e5bb401bb2b322ea85787ed05fc72c48e83b4
liujianhua <1769908045@qq.com>
2018-05-15 08:29:13 +0800

    dfd

--- gracly.sql
+++ gracly.sql
@@ -3,15 +3,15 @@
 
  Source Server         : local
  Source Server Type    : MySQL
- Source Server Version : 50715
+ Source Server Version : 80011
  Source Host           : localhost:3306
  Source Schema         : gracly
 
  Target Server Type    : MySQL
- Target Server Version : 50715
+ Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 13/05/2018 22:02:10
+ Date: 15/05/2018 08:27:34
 */
 
 SET NAMES utf8mb4;
@@ -22,19 +22,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `email` varchar(30) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `password` varchar(15) NOT NULL,
-  `image` varchar(40) DEFAULT NULL,
+  `email` varchar(30) COLLATE utf8_bin NOT NULL,
+  `password` varchar(20) COLLATE utf8_bin NOT NULL,
+  `name` varchar(15) COLLATE utf8_bin DEFAULT NULL,
+  `img` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1769908045@qq.com', '刘建华', 'a1769908045', '213123');
-INSERT INTO `user` VALUES ('2440031228@qq.com', '刘建瑞', 'a1769908045', '2134324');
+INSERT INTO `user` VALUES ('1769908045@qq.com', '1769908045', '刘建华', '1.png');
+INSERT INTO `user` VALUES ('4335666775@qq.com', 'adsh556', '张三', '2.png');
+INSERT INTO `user` VALUES ('67657464@sina.com', 'liujianhua', '李四', '3.pmg');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
