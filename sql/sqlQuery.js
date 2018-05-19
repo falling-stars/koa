1db9fbf89f2539a7fe33be38f7692384647361b9
liujianhua <1769908045@qq.com>
2018-05-19 18:44:00 +0800

    mysql

--- sql/sqlQuery.js
+++ sql/sqlQuery.js
@@ -1,23 +1,19 @@
 const fs = require('fs')
+const net = require('net')
 const {resolve} = require('path')
 const mysql = require('mysql')
 const config = JSON.parse(fs.readFileSync(resolve(__dirname, './DB.json')))
-let connection = mysql.createConnection(config)
-connection.connect()
-connection.on('error', error => {
-  if (error.code === 'PROTOCOL_CONNECTION_LOST') {
-    connection = mysql.createConnection(config)
-  } else {
-    throw error
-  }
-})
 module.exports = (sql) => {
   return new Promise((resolve, reject) => {
+    const connection = mysql.createConnection(config)
+    connection.connect()
     connection.query(sql, function (err, result) {
       if (err) {
         reject(err)
+        connection.end()
       } else {
         resolve(result)
+        connection.end()
       }
     })
   })
